2f7d4e5616b0561102489cf69c1ed242883cc6d7
VeranoDelfin2017 <Kaxan.app@gmail.com>
2017-07-14 17:25:06 -0500

    Integrancion a GitHub

---
+++ app/src/main/java/com/Kaxa/AplicacionKaxan/Ayuda3Activity.java
@@ -0,0 +1,13 @@
+package com.Kaxa.AplicacionKaxan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ayuda3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda3);
+    }
+}
---
+++ app/src/main/java/com/Kaxa/AplicacionKaxan/Ayuda4Activity.java
@@ -0,0 +1,13 @@
+package com.Kaxa.AplicacionKaxan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ayuda4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda4);
+    }
+}
